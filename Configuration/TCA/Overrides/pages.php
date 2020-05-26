4971995a78f453fdf6948fc068cb740d6daba12e
SvenJuergens <SvenJuergens@users.noreply.github.com>
2020-05-27 00:39:28 +0200

    [TASK] add Bootstrap Spacing Classes

---
+++ Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,10 @@
+<?php
+
+defined('TYPO3_MODE') or die();
+
+// TCEFORM
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    'content_wrapper',
+    'Configuration/TsConfig/Page/TCEFORM-bs-spacing-classes.tsconfig',
+    'Content_Wrapper: Bootstrap Spacing Classes'
+);
