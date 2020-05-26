ff3a9312107c841ecbde4b99ce3a0ac3a62e583a
SvenJuergens <SvenJuergens@users.noreply.github.com>
2020-05-27 00:54:10 +0200

    [TASK] add Bootstrap Sizing Classes

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -8,3 +8,11 @@ defined('TYPO3_MODE') or die();
     'Configuration/TsConfig/Page/TCEFORM-bs-spacing-classes.tsconfig',
     'Content_Wrapper: Bootstrap Spacing Classes'
 );
+
+// TCEFORM
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    'content_wrapper',
+    'Configuration/TsConfig/Page/TCEFORM-bs-sizing-classes.tsconfig',
+    'Content_Wrapper: Bootstrap Sizing Classes'
+);
+
