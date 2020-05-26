8643c0bb06cd6d34a7af138de6f1134438fd932e
SvenJuergens <SvenJuergens@users.noreply.github.com>
2020-05-27 01:09:58 +0200

    [TASK] add Bootstrap display Classes

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -16,3 +16,10 @@ defined('TYPO3_MODE') or die();
     'Content_Wrapper: Bootstrap Sizing Classes'
 );
 
+// TCEFORM
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    'content_wrapper',
+    'Configuration/TsConfig/Page/TCEFORM-bs-display-classes.tsconfig',
+    'Content_Wrapper: Bootstrap Display Classes'
+);
+
