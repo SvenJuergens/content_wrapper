b5ac034ee3d20f3a4b868f5329c1764ad88f2929
SvenJuergens <SvenJuergens@users.noreply.github.com>
2024-12-10 23:05:19 +0100

    [TASK] Version 3

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,10 +17,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Sven Jürgens',
     'author_email' => '',
     'state' => 'beta',
-    'version' => '2.0.0',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.30-12.4.99',
+            'typo3' => '12.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
